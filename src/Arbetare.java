9cd38e25cc48569f71ec42a1b24137da194ac8f6
Ken Granström <ken.granstrom@gmail.com>
2016-12-20 12:37:30 +0200

    Added totalwork calculation to Arbetare class

--- src/Arbetare.java
+++ src/Arbetare.java
@@ -3,20 +3,26 @@ import java.util.Map;
 public class Arbetare {
 	
 	private int time = 7 * 60; // 7h work time
+	private static final int workPerpiece = 5; // 5 minutes per 1 dimension, 1x1 would equal a total of 10 minutes work.
+	private int totalWorkamount;
 	
 	public void save() {
 	}
 
 
-	public void assignWorker(Produkt workAssignment, int workUnits) {
+	public void assignWork(Produkt workAssignment, int workUnits) {
 		
 		for (Map.Entry<Legobit, Integer> produktEntry : workAssignment.getPieces()) {
 			Legobit testLego = produktEntry.getKey();
 			int testAmount = produktEntry.getValue();
 			
-			System.out.println(testLego);
-			System.out.println(testAmount);
-		}
+			System.out.println("Assign work: ");
+			System.out.println("legobit length: " + testLego.getLength());
+			System.out.println("legobit height: " + testLego.getHeight());
+			totalWorkamount += (testLego.getLength() * workUnits);
 
+			
+		}
+		System.out.println("Your product will require a total of: " + totalWorkamount + "minutes to complete!");
 	}
 }
